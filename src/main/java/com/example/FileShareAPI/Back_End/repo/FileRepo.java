a44d6fcf34f15fd538919eda68e85229c8f8e482
Marten221 <144438767+Marten221@users.noreply.github.com>
2024-09-04 14:39:05 +0300

    Add 'isPublic' field to File entity and update access control
    - Added 'isPublic' boolean field to the File entity to allow users to specify if their file is public.
    - Implemented 'isAccessible' method in FileSpecifications class to ensure users only access files they are authorized to view.
    - Integrated 'isAccessible' method into 'getFilesByKeyword' and 'getFileExtensions' methods for enhanced file access control.
    - Removed 'getFileExtensions' method from FileRepository, as its functionality was replaced by the new specification-based approach.

--- src/main/java/com/example/FileShareAPI/Back_End/repo/FileRepo.java
+++ src/main/java/com/example/FileShareAPI/Back_End/repo/FileRepo.java
@@ -10,8 +10,4 @@ import java.util.Set;
 
 @Repository
 public interface FileRepo extends JpaRepository<File, String>, JpaSpecificationExecutor<File> {
-
-    @Query(value = "SELECT distinct file_extension FROM files f", nativeQuery = true) //TODO: Where userid == Loggedinuser
-    Set<String> getFileExtensions();
-
 }
