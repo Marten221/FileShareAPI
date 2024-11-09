ff929ce7d3e41d2e4f3cfc8decd067739922dacb
Marten221 <marten.ojasaar@ut.ee>
2024-11-09 18:44:09 +0200

    refactor: Reorganize FileService methods and utilities
    - Moved `getFileExtensions` method from `FileService` to `UserService` for better alignment with user-related operations.
    - Relocated `toDto` method from `FileService` to `File` class, as it is directly related to `File` functionality.
    - Created `FileUtils` class to centralize file-related utility functions.
      - Transferred the following methods from `FileService` to `FileUtils`:
        - `stringIsNullOrBlank`
        - `bytesToHumanReadable`
        - `truncateString`
        - `findSorting`
        - `saveFile`

--- src/main/java/com/example/FileShareAPI/Back_End/service/FileService.java
+++ src/main/java/com/example/FileShareAPI/Back_End/service/FileService.java
@@ -22,13 +22,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import static com.example.FileShareAPI.Back_End.constant.Constant.FILE_DIRECTORY;
-import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static utils.FileUtils.*;
 
 @Service
 @RequiredArgsConstructor
@@ -36,15 +32,7 @@ public class FileService {
     private final FileRepo fileRepo;
     private final UserRepo userRepo;
 
-    private static final Map<String, Sort> SORTING_MAP = new HashMap<>();
-    static {
-        SORTING_MAP.put("name_ascending", Sort.by("fileName").ascending()); // Specification doesn't support snake case?? only read file from file_name
-        SORTING_MAP.put("name_descending", Sort.by("fileName").descending()); // Specification needs the class field name not the field name from the database table
-        SORTING_MAP.put("date_ascending", Sort.by("timestamp").ascending());
-        SORTING_MAP.put("date_descending", Sort.by("timestamp").descending());
-        SORTING_MAP.put("size_ascending", Sort.by("sizeBytes").ascending());
-        SORTING_MAP.put("size_descending", Sort.by("sizeBytes").descending());
-    }
+
 
     public FileDto createFile(String id,
                               MultipartFile file,
@@ -53,103 +41,30 @@ public class FileService {
                               Boolean isPublic) throws IOException {
         assert customFilename != null;
         assert description != null;
-        if (customFilename.length() > 255) customFilename = customFilename.substring(0, 255); //TODO: check if it is null before
-        if (description.length() > 1000) description = description.substring(0, 1000);
+        customFilename = truncateString(customFilename, 255); //truncate method also checks for null
+        description = truncateString(description, 255);
 
         User fileOwner = userRepo.getReferenceById(id);
-        String originalFilename = getOriginalFilename(file);
+        String originalFilename = file.getOriginalFilename();
+
+        assert originalFilename != null;
         String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
         long fileSize = file.getSize();
 
-        File fileObject = createFileObject(fileOwner,
+        File fileObject = new File(fileOwner,
                 extension,
                 originalFilename,
                 fileSize,
                 bytesToHumanReadable(fileSize),
                 customFilename,
                 description,
-                isPublic);// Creating the file and setting its values
+                isPublic);// Creating the file object
 
         fileRepo.save(fileObject); // saving the file to the database and file system
         String newFileName = fileObject.getFileId() + "." + extension;
         saveFile(newFileName, file, id);
 
-        return fileToDto(fileObject, false);
-    }
-
-    private static String getOriginalFilename(MultipartFile file) {
-        String originalFilename = file.getOriginalFilename();
-        assert originalFilename != null; //TODO: remove for production
-        return originalFilename;
-    }
-
-    private static File createFileObject(User fileOwner,
-                                         String extension,
-                                         String originalFilename,
-                                         long size,
-                                         String sizeHumanReadable,
-                                         String customFileName,
-                                         String desc,
-                                         boolean isPublic) {
-        File fileObject = new File();
-        originalFilename = originalFilename.substring(0, originalFilename.lastIndexOf(".")); //TODO: if the file has no name/extension, an error occurs
-        if (originalFilename.length() > 255) originalFilename = originalFilename.substring(0, 255);
-
-        fileObject.setUser(fileOwner);
-        fileObject.setFileExtension(extension);
-        fileObject.setFileName(originalFilename); //TODO: remove file ext from the original filename
-        fileObject.setSizeBytes(size);
-        fileObject.setSizeHumanReadable(sizeHumanReadable);
-        fileObject.setFileName(stringIsNullorBlank(customFileName) ? originalFilename : customFileName); // If no custom name is given, use the original file Name
-        fileObject.setDescription(desc);
-        fileObject.setIsPublic(isPublic);
-        fileObject.setTimestamp(LocalDateTime.now());
-
-        return fileObject;
-    }
-
-
-    private void saveFile(String fileName, MultipartFile file, String userId) throws IOException {
-        String customFile_Directory = FILE_DIRECTORY + userId + "/"; // Custom folder for each user
-        Path fileStorageLocation = Paths.get(customFile_Directory).toAbsolutePath().normalize();
-
-        if (!Files.exists(fileStorageLocation)) {
-            Files.createDirectories(fileStorageLocation);
-        } // If there is no such directory, create it
-
-        Files.copy(file.getInputStream(), fileStorageLocation.resolve(fileName), REPLACE_EXISTING);
-    }
-
-    private FileDto fileToDto(File fileObject, boolean previewBool) {
-        String descriptionString = fileObject.getDescription();
-        String description;
-        if (previewBool && descriptionString.length() > 100) description = descriptionString.substring(0, 100) + "..."; // If the front end request data for cards, then the full desc is not needed
-        else description = descriptionString;
-
-        return new FileDto(
-                fileObject.getFileId(),
-                fileObject.getFileName(),
-                fileObject.getFileExtension(),
-                fileObject.getSizeHumanReadable(),
-                description,
-                fileObject.getTimestamp(),
-                fileObject.getIsPublic()
-        );
-    }
-
-    public static String bytesToHumanReadable(long bytes) {
-        if (bytes < 1000) {
-            return bytes + " B";
-        }
-        int unit = 1000;
-        String[] units = {"KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"};
-
-        // Calculate the index of the unit
-        int index = (int) (Math.log(bytes) / Math.log(unit));
-        // Round the bytes to two decimal places
-        double value = bytes / Math.pow(unit, index);
-
-        return String.format("%.2f %s", value, units[index - 1]);
+        return fileObject.toDto(false);
     }
 
     public byte[] getFileContent(String fileId) throws IOException { //TODO: check if the user has access to this file!
@@ -167,8 +82,8 @@ public class FileService {
         return header;
     }
 
-    private Path getFilePath(String fileName) {
-        File file = fileRepo.getReferenceById(fileName);
+    private Path getFilePath(String fileId) {
+        File file = fileRepo.getReferenceById(fileId);
         String fileLocation = FILE_DIRECTORY + file.getUser().getUserId() + "/" + file.getFileId() + "." + file.getFileExtension();
         return Paths.get(fileLocation);
     }
@@ -192,26 +107,15 @@ public class FileService {
                 .and(FileSpecifications.hasExtension(extension));
 
         Page<File> filesPage = fileRepo.findAll(spec, pageable);
-        return filesPage.map(file -> fileToDto(file, true));
+        return filesPage.map(file -> file.toDto(true));
     }
 
-    private Sort findSorting(String sorting) {
-        return SORTING_MAP.getOrDefault(sorting, Sort.by("file_name").ascending());
-    }
 
     public FileDto getFileDescription(String fileId) {
-        return fileToDto(fileRepo.getReferenceById(fileId), false);
+        return fileRepo.getReferenceById(fileId).toDto(false);
     }
 
 
-    public Set<String> getFileExtensions(String userId) {
-        Specification<File> spec = Specification.where(FileSpecifications.isAccessible(userId));
-        return fileRepo.findAll(spec).stream()
-                .map(File::getFileExtension)
-                .collect(Collectors.toSet());
-    }
-
-    //TODO: testt!!
     public FileDto updateFile(String userId,
                               String fileId,
                               MultipartFile newFile,
@@ -223,7 +127,8 @@ public class FileService {
         if (!newFile.isEmpty()) {
             deleteFile(userId, fileId); //If a new file was uploaded, delete the previous file.
 
-            String originalFilename = getOriginalFilename(newFile);
+            String originalFilename = newFile.getOriginalFilename();
+            assert originalFilename != null;
             String extension = originalFilename.substring(originalFilename.lastIndexOf(".") + 1);
             fileToUpdate.setFileExtension(extension);
 
@@ -249,7 +154,7 @@ public class FileService {
         //TODO: create another db table for logging operations made with the file
         File updatedFile = fileRepo.save(fileToUpdate);
 
-        return fileToDto(updatedFile, false);
+        return updatedFile.toDto(false);
     }
 
     public void deleteFile(String userId, String fileId) throws IOException {
@@ -264,7 +169,6 @@ public class FileService {
 
 
 
-
     private File verifyFileOwnership(String userId, String fileId) {
         User fileOwner = userRepo.getReferenceById(userId);
         File fileToUpdate = fileRepo.findByUserAndFileId(fileOwner, fileId);
@@ -273,7 +177,4 @@ public class FileService {
         return fileToUpdate;
     }
 
-    private static boolean stringIsNullorBlank(String string) {
-        return string == null || string.isBlank();
-    }
 }
